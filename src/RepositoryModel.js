5235eb94b30db93fa05d34ab63c1590f84092b0c
Carolina Oliveira <caroliveira.ti@gmail.com>
2020-06-28 06:47:48 -0300

    create repository model

---
+++ src/RepositoryModel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+
+const ContributorSchema = new mongoose.Schema(
+  {
+    login: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    contributions: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false }
+);
+
+const PullRequestSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  responsable: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+},
+{ _id: false });
+
+const RepositorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  language: {
+    type: String,
+    required: true,
+  },
+  contributors: {
+    type: [ContributorSchema],
+    required: true,
+  },
+  pullRequests: {
+    type: [PullRequestSchema],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Repository", RepositorySchema);
