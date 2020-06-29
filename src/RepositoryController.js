9b04fa7d10897d4cb80cb87e67e35ee879417043
Carolina Oliveira <caroliveira.ti@gmail.com>
2020-06-28 23:27:49 -0300

    error treatment done

--- src/RepositoryController.js
+++ src/RepositoryController.js
@@ -1,21 +1,30 @@
 const Repository = require("./RepositoryModel");
 
+const status = 500;
+
 module.exports = {
   async all(req, res) {
     try {
       const repositories = await Repository.find({});
       return res.json(repositories);
     } catch (err) {
-      console.log(err);
+      return res.status(status).send();
     }
   },
 
   async create(req, res) {
     try {
+      const check = await Repository.findOne({ url: req.body.url });
+      if (check) {
+        return res.status(403).send();
+      }
+      if (req.body.pullRequests.lenght > 3) {
+        return res.status(400).send();
+      }
       const repository = await Repository.create(req.body);
       return res.json(repository);
     } catch (err) {
-      console.log(err);
+      return res.status(status).send();
     }
   },
 };
